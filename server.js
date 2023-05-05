d9e15f07c84349b15944c327cc0d76757a3a18b9
anil <ak26122k@gmail.com>
2023-05-05 11:07:59 -0700

    category api

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
 //configure env
@@ -23,6 +25,8 @@ app.use(express.json());
 // routes
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
