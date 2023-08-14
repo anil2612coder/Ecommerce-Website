ad94745e2e4778984ee422a26db4de7e7de4657e
Anil <ak26122k@gmail.com>
2023-08-14 10:22:30 +0530

    some backend url left now i change

--- client/src/components/Routes/AdminRoute.jsx
+++ client/src/components/Routes/AdminRoute.jsx
@@ -11,7 +11,7 @@ export default function AdminRoute() {
   useEffect(() => {
     const authCheck = async () => {
       const res = await axios.get(
-        `http://localhost:8080/api/v1/auth/admin-auth`
+        `https://ecommerce-website-r5ak.onrender.com/api/v1/auth/admin-auth`
       );
       if (res.data.ok) {
         setOk(true);
